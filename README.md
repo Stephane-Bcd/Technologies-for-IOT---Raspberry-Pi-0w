f36e16fd25627ede0479746d11c6a6c183d070c2
Stephane Bcd <stephane.boucaud@outlook.fr>
2020-01-14 18:22:24 +0100

    continuing reorganisation and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Technologies for IOT - Raspberry Pi 0w
+
+
+# Steps to make it work
+
+Install needed packages using command:
+sh bash.sh
+
--- raspberry/rssaggregator/test.py
+++ raspberry/rssaggregator/test.py
@@ -3,3 +3,5 @@ from rssaggregator import RssAggregator
 
 rssobject = RssAggregator("https://www.gdacs.org/xml/rss_24h.xml")
 rssobject.parse()
+
+
