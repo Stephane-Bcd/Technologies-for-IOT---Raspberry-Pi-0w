d9a424d17084d52fb707fe38ce46487d6b4bb0cd
Stephane Bcd <stephane.boucaud@outlook.fr>
2020-01-14 17:07:33 +0100

    changed solution to feedparser

--- rasppi0wiot/functions.py
+++ rasppi0wiot/functions.py
@@ -1,14 +1,33 @@
-import requests
-
-'''
-	Function to retrieve rss data
-'''
-def get_rss_json (get_url = "https://www.gdacs.org/xml/rss_24h.xml"):
-	r = requests.get(get_url)
-	res_text = r.text
-	
-	
-	
-	print(res_text)
+import feedparser
 
 
+class WhizRssAggregator():
+	feedurl = ""
+
+	def __init__(self, paramrssurl):
+		print(paramrssurl)
+		print("")
+		self.feedurl = paramrssurl
+
+	def parse(self):
+		thefeed = feedparser.parse(self.feedurl)
+
+		print("Getting Feed Data")
+		print(thefeed.feed.get("title", ""))
+		print(thefeed.feed.get("link", ""))
+		print(thefeed.feed.get("description", ""))
+		print(thefeed.feed.get("published", ""))
+		print(thefeed.feed.get("published_parsed", thefeed.feed.published_parsed))
+		print("\n\n")
+
+		for thefeedentry in thefeed.entries:
+			print("__________")
+			print(thefeedentry.get("guid", ""))
+			print(thefeedentry.get("title", ""))
+			print(thefeedentry.get("link", ""))
+			print(thefeedentry.get("description", ""))
+			print("__________")
+
+
+rssobject = WhizRssAggregator("https://www.gdacs.org/xml/rss_24h.xml")
+rssobject.parse()
