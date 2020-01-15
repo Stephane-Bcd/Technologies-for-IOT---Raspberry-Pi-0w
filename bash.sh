be1f2880688e201a952378f5b30d37b8d8a57caa
Stephane Bcd <stephane.boucaud@outlook.fr>
2020-01-15 08:11:03 +0100

    config for grafana influx db docker compose

--- bash.sh
+++ bash.sh
@@ -21,6 +21,36 @@
 # git clone "https://github.com/Stephane-Bcd/Technologies-for-IOT---Raspberry-Pi-0w" "./Tech for IOT"
 
 # To install python requirements (file at the root of this git project):
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+
+sudo docker run hello-world
+
+sudo docker network create iot-labs
+
+sudo docker-compose -f $PWD/raspberry/influxdbgrafana/docker-compose.yml up -d
+
+sudo docker ps -a
+
+
+
 sudo apt install python3
 sudo apt install python3-pip
 pip3 install -r requirements.txt
