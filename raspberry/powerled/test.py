b3f704c7248d6fb18ef1cbf7fbfa7db228b24d1c
Stephane Bcd <stephane.boucaud@outlook.fr>
2020-01-15 06:35:50 +0100

    light up and then light down

--- raspberry/powerled/test.py
+++ raspberry/powerled/test.py
@@ -1,8 +1,22 @@
 
 from models import GPIOController
+import time
 
 gpc = GPIOController()
 
 print (gpc.test_environment)
 
 gpc.switch_leds_states()
+
+
+time.sleep(30)
+
+gpc.switch_leds_states(regions_booleans = {
+			"Eastern Asia": False,
+			"Europe": False,
+			"Africa": False,
+			"Oceania": False,
+			"Northern America": False,
+			"South America": False,
+			"Western Asia": False
+		})
